79cca8ef634235022c68157d8046051f8cf6858d
phihungnguyen1402-byte <phihungnguyen1402@gmail.com>
2026-06-08 09:29:20 +0700

    Time: 0 ms (100%), Space: 12.4 MB (58.71%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -2,23 +2,14 @@
 #include <algorithm> // Thư viện để dùng hàm tìm max nhanh
 
 using namespace std;
-
 class Solution {
 public:
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
-        // Tìm số kẹo lớn nhất hiện tại bằng hàm max_element
-        // Lưu ý: max_element trả về con trỏ (iterator), nên cần thêm dấu * ở trước để lấy giá trị
-        int maxCandies = *max_element(candies.begin(), candies.end());
-        
-        vector<bool> result;
-        
-        // Duyệt qua từng bé và kiểm tra
-        for (int candy : candies) {
-            // Biểu thức (candy + extraCandies >= maxCandies) sẽ tự động trả về true hoặc false
-            // Nên mình đưa thẳng vào hàm push_back luôn cho gọn
-            result.push_back(candy + extraCandies >= maxCandies);
-        }
-        
-        return result;
+   int maxCandies=*max_element(candies.begin(),candies.end());
+   vector<bool> result;
+   for(int candy:candies){
+    result.push_back(candy+extraCandies>= maxCandies);
+   }
+   return result;
     }
 };

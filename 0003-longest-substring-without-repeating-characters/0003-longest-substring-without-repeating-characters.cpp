03060866edd651216565986d22d01cd6e35bd310
phihungnguyen1402-byte <phihungnguyen1402@gmail.com>
2026-06-08 15:53:31 +0700

    Time: 16 ms (32.98%), Space: 14.5 MB (12.56%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         unordered_set<char> charset;
-    int left =0;
-    int maxlong=0;
-    for (int right=0;right <s.length();right++){
-        while (charset.count (s[right])>0){
-            charset.erase(s[left]);
-            left++;
+    int left=0;
+    int maxlength=0;
+    for (int right=0; right < s.length(); right++){
+        while(charset .count(s[right])>0){
+        charset.erase(s[left]);
+        left++;
         }
         charset.insert(s[right]);
-        maxlong=max(maxlong, right-left+1);
-        }
-    return maxlong;
+        maxlength =max(maxlength,right-left+1);
     }
-};
+    return maxlength;
+    }
+    };

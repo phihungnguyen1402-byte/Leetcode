ece0bd0570fdd2d43c06870b45672e14de32ef84
phihungnguyen1402-byte <phihungnguyen1402@gmail.com>
2026-06-08 16:12:12 +0700

    Time: 21 ms (20.41%), Space: 14.1 MB (34.65%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -4,14 +4,14 @@ public:
         unordered_set<char> charset;
     int left=0;
     int maxlength=0;
-    for (int right=0; right < s.length(); right++){
-        while(charset .count(s[right])>0){
-        charset.erase(s[left]);
-        left++;
+    for (int right=0;right<s.length();right++){
+        while(charset.count(s[right])>0){
+            charset.erase(s[left]);
+            left++;
         }
         charset.insert(s[right]);
-        maxlength =max(maxlength,right-left+1);
+        maxlength=max(maxlength,right-left+1);
     }
     return maxlength;
     }
-    };
+};

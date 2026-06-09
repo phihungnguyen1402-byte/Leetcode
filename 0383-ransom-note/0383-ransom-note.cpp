8c267211fb9667923fd8c34b6c3e19ba85a9a7cd
phihungnguyen1402-byte <phihungnguyen1402@gmail.com>
2026-06-09 11:05:20 +0700

    Time: 1 ms (63.44%), Space: 11.8 MB (73.12%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canConstruct(string ransomnote, string magazine) {
+     vector<int> lettercount(26,0);
+     for (char c:magazine){
+        lettercount[c-'a']++;
+
+     }
+     for(char c:ransomnote){
+        lettercount[c-'a']--;
+        if(lettercount[c-'a']<0){
+        
+     return false;
+        }
+     } return true;
+    }
+};

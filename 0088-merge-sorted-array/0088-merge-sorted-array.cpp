1914839af417624c55fd629b779a8395b66afe95
phihungnguyen1402-byte <phihungnguyen1402@gmail.com>
2026-06-11 23:12:58 +0700

    Time: 0 ms (100%), Space: 12.4 MB (8.32%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -5,15 +5,14 @@ public:
   int j=n-1;
   int k=m+n-1;
   while(j>=0){
-    if( i>=0&& nums1[i]>nums2[j]){
+    if (i>=0 && nums1[i]>nums2[j]){
         nums1[k]=nums1[i];
         i--;
     }else{
         nums1[k]=nums2[j];
         j--;
-    }
-    k--;
-  }
+    
+  }k--;
  }
- };
-  
+ }
+  };

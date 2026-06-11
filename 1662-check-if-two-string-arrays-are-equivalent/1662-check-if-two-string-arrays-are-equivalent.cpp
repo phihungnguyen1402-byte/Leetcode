76577e4c945f94e60431b9c21253ba7cf8cd1d17
phihungnguyen1402-byte <phihungnguyen1402@gmail.com>
2026-06-11 22:21:27 +0700

    Time: 0 ms (100%), Space: 15.2 MB (13.04%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
@@ -1,24 +1,14 @@
-
-
-using namespace std;
-
-class Solution {
-public:
-    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
-        string s1 = ""; // Chuỗi để dán túi 1
-        string s2 = ""; // Chuỗi để dán túi 2
-
-        // Bước 1: Dán các mảnh trong word1
-        for (string s : word1) {
-            s1 += s; 
+class Solution{
+    public:
+    bool arrayStringsAreEqual(vector<string>&word1,vector<string>&word2){
+        string s1="";
+        string s2="";
+        for(string s:word1){
+            s1+=s;
         }
-
-        // Bước 2: Dán các mảnh trong word2
-        for (string s : word2) {
-            s2 += s;
+        for(string s:word2){
+            s2+=s;
         }
-
-        // Bước 3: So sánh hai kết quả cuối cùng
-        return s1 == s2;
-    }
-};
+    return s1==s2;
+    }  
+};    

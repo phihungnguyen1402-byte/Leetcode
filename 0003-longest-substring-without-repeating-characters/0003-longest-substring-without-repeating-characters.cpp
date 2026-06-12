7212d3137f13291b9b237704280e29dbab504336
phihungnguyen1402-byte <phihungnguyen1402@gmail.com>
2026-06-12 14:59:05 +0700

    Time: 20 ms (22.21%), Space: 14.4 MB (20.43%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
-    int lengthOfLongestSubstring(string size) {
+    int lengthOfLongestSubstring(string s) {
         unordered_set<char> charset;
-    int left=0;
-    int maxlength=0;
-    for(int right=0;right<size.length();right++){
-        while(charset.count(size[right])>0){
-            charset.erase(size[left]);
-            left++;
+    int left = 0;   
+        int maxlength = 0;
+
+        for (int right = 0; right < s.length();right++) {
+            char wordnew = s[right];
+            while (charset.contains(wordnew)){
+
+                charset.erase(s[left]); 
+                left++; 
+            }
+            charset.insert(wordnew);
+            maxlength = max(maxlength, right - left + 1);
         }
-    charset.insert(size[right]);
-    maxlength=max(maxlength,right-left+1);
-    } return maxlength;
+        return maxlength;
     }
 };

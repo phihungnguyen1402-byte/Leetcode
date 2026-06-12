5f90e481417a8865640718b00dffdb9d2a34d479
phihungnguyen1402-byte <phihungnguyen1402@gmail.com>
2026-06-12 11:30:20 +0700

    Time: 1 ms (57.97%), Space: 10.2 MB (73.71%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     bool judgeCircle(string moves) {
-     int x=0,y=0;
-     for (char move : moves){
-        if (move =='D'){
-            y--;
-        }else if (move =='U'){
+    int x=0;
+    int y=0;
+    for(char move : moves){
+        if(move=='U'){
             y++;
-        }else if (move =='L'){
+        }else if(move=='D'){
+            y--;
+        }else if(move =='L'){
             x--;
-        }else if( move =='R'){
+        }else if(move =='R'){
             x++;
         }
-     }
-        return (x==0 && y==0);
-     }
-    };
+    }return (x==0&& y==0);
+    }
+};

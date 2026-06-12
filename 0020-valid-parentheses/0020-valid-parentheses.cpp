e938ebe60969695ab6ce702bb00d82b45b4bf19b
phihungnguyen1402-byte <phihungnguyen1402@gmail.com>
2026-06-12 09:46:02 +0700

    Time: 0 ms (100%), Space: 8.7 MB (88.47%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -3,23 +3,23 @@ class Solution{
     public:
     bool isValid(string s){
         stack<char> st; 
-        for (char c : s) {
-            if (c == '(' || c == '{' || c == '[') {
+        for(char c:s){
+            if(c=='('||c=='{'||c=='['){
                 st.push(c);
-            } else {
-                if (st.empty()) {
+            }else{
+                if(st.empty()){
                     return false;
                 }
-            char top = st.top();
-            if ((c == ')' && top == '(') ||
-                (c == '}' && top == '{') ||
-                (c == ']' && top == '[')) {
-                st.pop();
-             } else {
-                 return false; 
-             }
+                 char top=st.top();
+                if((c==')'&&top=='(')||
+                    (c=='}'&&top=='{')||
+                    (c==']'&&top=='[')){
+                        st.pop();
+                }else{
+                return false;
+                }
             }
         }
         return st.empty();
     }
-};
+};   

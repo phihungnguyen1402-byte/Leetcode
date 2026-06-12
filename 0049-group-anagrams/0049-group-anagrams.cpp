a09b8567e63f34cdf820bf7f1f03376698833bd1
phihungnguyen1402-byte <phihungnguyen1402@gmail.com>
2026-06-12 17:19:45 +0700

    Time: 12 ms (83.4%), Space: 27.2 MB (24.14%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-         unordered_map<string, vector<string>> map;
-   for(string word:strs){
-    string sortword=word;
-    sort(sortword.begin(),sortword.end());
-    map[sortword].push_back(word);
-   }
-   vector<vector<string>> result;
-   for(auto&pair:map){
-    result.push_back(pair.second);
-   }
-   return result;
+        unordered_map<string, vector<string>> map; 
+        for(string word : strs) {
+            string barcode(26, '0'); 
+            for(char c : word) {
+                barcode[c - 'a']++; 
+            }
+            map[barcode].push_back(word);
+        }
+        vector<vector<string>> result;
+        for(auto& pair : map) {
+            result.push_back(pair.second);
+        }
+        return result;
     }
 };

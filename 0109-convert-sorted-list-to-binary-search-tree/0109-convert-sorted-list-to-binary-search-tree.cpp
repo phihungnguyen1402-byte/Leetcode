39544a00345bf57a75c65ab0c640c800be461a91
Nguyễn Phi Hùng <phihungnguyen1402@gmail.com>
2026-06-22 09:46:36 +0700

    Time: 3 ms (96.45%), Space: 33.4 MB (18.34%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     TreeNode* sortedListToBST(ListNode* head) {
-        if (head == nullptr) {
+        vector<int> values;
+        while (head != nullptr) {
+            values.push_back(head->val);
+            head = head->next;
+        }
+        return buildTree(values, 0, values.size() - 1);
+    }
+private:
+    TreeNode* buildTree(const vector<int>& values, int left, int right) {
+        if (left > right) {
             return nullptr;
         }
-        if (head->next == nullptr) {
-            return new TreeNode(head->val);
-        }
-        ListNode* slow = head;        
-        ListNode* fast = head;        
-        ListNode* prev = nullptr;     
-        while (fast != nullptr && fast->next != nullptr) {
-            prev = slow;
-            slow = slow->next;       
-            fast = fast->next->next;  
-        }
-        prev->next = nullptr; 
-        TreeNode* root = new TreeNode(slow->val);
-        root->left = sortedListToBST(head);
-        root->right = sortedListToBST(slow->next);
+        int mid = left + (right - left) / 2; 
+        TreeNode* root = new TreeNode(values[mid]);
+        root->left = buildTree(values, left, mid - 1);
+        root->right = buildTree(values, mid + 1, right);
         return root;
     }
 };

9550e40123b2f08697eb70968d51b99188ae3853
Nguyễn Phi Hùng <phihungnguyen1402@gmail.com>
2026-06-23 09:41:03 +0700

    Time: 0 ms (100%), Space: 8.5 MB (67.65%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <cmath> // Để dùng hàm abs()
+using namespace std;
+class Solution {
+public:
+    string makeGood(string s) {
+        string result = "";
+        for (char c : s) {
+            if (!result.empty() && abs(result.back() - c) == 32) {
+                result.pop_back(); 
+            } else {
+                result.push_back(c);
+            }
+        }
+        return result;
+    }
+};
